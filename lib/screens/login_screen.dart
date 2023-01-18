a8b65eb075cc5ef7896c7cc4e18ec8391610b5aa
kashyapsandesh <sandeshghimire202@gmail.com>
2023-01-18 22:49:01 +0545

    login_page added

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import '../components/constants/const.dart';
+import '../components/constants/textform.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -7,6 +8,32 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: body,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Logo(),
+              70.heightBox,
+              login.text.size(20).white.fontFamily("mont_bold").make(),
+              25.heightBox,
+              Container(
+                child: Column(
+                  children: [
+                    CTextForm(hint: "Enter your email"),
+                    10.heightBox,
+                    CTextForm(hint: "Enter your password"),
+                    10.heightBox,
+                    CButton(ctext: "Login", onPress: () {}),
+                    10.heightBox,
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
